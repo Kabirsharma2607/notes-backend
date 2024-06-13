b81e00a0102454d91590265baf5128db00de74b8
Kabirsharma2607 <kabirsharma2607@gmail.com>
2024-06-14 00:32:42 +0530

    Added signin route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,8 +2,15 @@ const express = require("express");
 
 const User = require("../models/userModel.js");
 const zod = require("zod");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 const usersRouter = express.Router();
+
+const signinZod = zod.object({
+  email: zod.string().email(),
+  password: zod.string(),
+});
 
 const userZod = zod.object({
   firstName: zod.string(),
@@ -15,7 +22,7 @@ const userZod = zod.object({
 usersRouter.post("/signup", async (req, res) => {
   try {
     const body = req.body;
-    console.log(body);
+    //console.log(body);
     const { success } = userZod.safeParse(body);
     if (!success) {
       return res.json({
@@ -24,7 +31,7 @@ usersRouter.post("/signup", async (req, res) => {
       });
     }
     const { email, password, firstName, lastName } = body;
-    console.log(email + " " + password);
+    //console.log(email + " " + password);
     const existingUser = await User.findOne({
       email,
     });
@@ -41,14 +48,64 @@ usersRouter.post("/signup", async (req, res) => {
       lastName,
       password,
     });
-    console.log(user);
+    //console.log(user);
+    const payload = {
+      _id: user._id,
+      email: user.email,
+    };
+    const token = jwt.sign(payload, process.env.JWT_SECRET);
     return res.status(200).json({
       success: true,
       message: "User registered successfully",
+      token: token,
     });
   } catch (error) {
     console.log(error.message);
     return res.status(400);
+  }
+});
+
+usersRouter.get("/signin", async (req, res) => {
+  try {
+    const body = req.body;
+    const { success } = signinZod.safeParse(body);
+    if (!success) {
+      return res.json({
+        success: false,
+        message: "Invalid parameters",
+      });
+    }
+
+    const user = await User.findOne({
+      email: body.email,
+    });
+    if (!user) {
+      return res.json({
+        success: false,
+        message: "User does not exist",
+      });
+    }
+
+    if (body.password !== user.password) {
+      return res.json({
+        success: false,
+        message: "Passwords does not match",
+      });
+    }
+    const payload = {
+      _id: user._id,
+      email: user.email,
+    };
+    const token = jwt.sign(payload, process.env.JWT_SECRET);
+    return res.status(200).json({
+      sucess: true,
+      message: "User signed in successfully",
+      token: token,
+    });
+  } catch (error) {
+    return res.status(504).json({
+      message: "Error",
+    });
   }
 });
 
